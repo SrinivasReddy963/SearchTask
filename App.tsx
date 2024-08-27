50682e3e3b5c72d072cdfb45dd94925d189ba325
SrinivasReddy963 <142573174+SrinivasReddy963@users.noreply.github.com>
2024-08-27 14:43:50 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Search from './components/Search';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
+};
+
+export default App;
